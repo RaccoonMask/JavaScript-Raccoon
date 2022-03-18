0cc3da27b269d5df0e57963a9ccbe1b4ec6271bb
Raccoon Mask <raccoonmask.dev@gmail.com>
2022-03-17 23:12:48 -0500

    Se genera el codigo del cuadrado de forma estatica y se comprueban los valores, todo ok

---
+++ figuras.js
@@ -0,0 +1,11 @@
+//****Codigo del cuadrado
+
+//lado del cuadrado
+const ladoCuadrado = 5
+console.log(`Los lados del cuadrado miden ${ladoCuadrado} cm`)
+//perimetro del cuadrado
+const perimetroCuadrado = ladoCuadrado*4
+console.log(`El perimetro de mi cuadrado es 4 lados * ${5} cm = ${perimetroCuadrado} cm`)
+//area del cuadrado
+const areaCuadrado = ladoCuadrado*ladoCuadrado
+console.log(`El area de mi cudrado es ${ladoCuadrado} cm * ${ladoCuadrado} cm = ${areaCuadrado} cm^2`)
